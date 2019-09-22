b8e42730b6c2a685fa8714f728b51b3c309d18e2
Yunrui Li <yunrui.li@shopee.com>
2019-09-23 02:32:50 +0800

    add args for saving result of crawling followers

--- util/datasaver.py
+++ util/datasaver.py
@@ -26,9 +26,24 @@ class Datasaver:
         else:
             file_commenters = os.path.join(Settings.profile_commentors_location, username + "_commenters.txt")
 
-        print(file_commenters)
         with open(file_commenters, 'w') as fc:
             for line in user_commented_list:
+                fc.write(line)
+                fc.write("\n")
+        fc.close()
+    def save_followers_txt(username, followers_list):
+        # 
+        check_folder(Settings.followers_location)
+        #
+        if (Settings.followers_file_with_timestamp):
+            file_followers = os.path.join(Settings.followers_location,
+                                           username + "_commenters_" + datetime.datetime.now().strftime(
+                                               "%Y-%m-%d %H-%M-%S") + ".txt")
+        else:
+            file_followers = os.path.join(Settings.followers_location, username + "_commenters.txt")
+
+        with open(file_followers, 'w') as fc:
+            for line in followers_list:
                 fc.write(line)
                 fc.write("\n")
         fc.close()
